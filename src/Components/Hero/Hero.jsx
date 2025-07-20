69e0a951a5bfe002022ef10df5335848df465ac5
anbuchezhiyan <anbuchezhiyan.am2023ai-ds@sece.ac.in>
2025-07-20 11:57:21 +0530

    Completed the hero section of the portfolio. Added styles and minimal animations

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,10 +6,12 @@ const Hero = () => {
     return (
         <div className="hero">
             <img src = {profile_img} alt = "" />
-            <h1><span>I'm Anbuchezhiyan</span>, from India</h1>
+            <h1><span>I'm Anbuchezhiyan</span>, from <span className ="IN">In</span><span className="D">d</span><span className="IA">ia</span></h1>
             <p>I am an aspiring backend developer from India</p>
-            <div className = "hero-connect"> Connect with me</div>
-            <div className = "hero-resume">My Resume</div>
+            <div className='hero-action'>
+                <div className = "hero-connect"> Connect with me</div>
+                <div className = "hero-resume">My Resume</div>
+            </div>
         </div>
     );
 };
